2540f42edf8730dc4528b216ade02249e4a39222
Dominik Kulon <dkulon95@gmail.com>
2016-02-22 23:03:26 +0000

    Populate the collection view

--- hiTour/FeedController.swift
+++ hiTour/FeedController.swift
@@ -12,19 +12,31 @@ import UIKit
 
 class FeedController: UICollectionViewController {
     
+    let prototypeData = PrototypeDatum.getAllData
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-//    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-//        
-//    }
-//    
-//    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        
-//    }
-//    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("FeedControllerCell", forIndexPath: indexPath) as! FeedControllerCell
+        let datum = self.prototypeData[indexPath.row]
+        
+        cell.imageViewFeed?.image = UIImage(named: datum.imageName)
+        cell.imageViewFeed?.contentMode = .ScaleAspectFill
+        cell.labelTitle.text = datum.title
+        
+        return cell
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return self.prototypeData.count
+    }
+    
 //    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+//        // TODO: launch the detail view
 //    }
     
     
--- hiTour/FeedControllerCell.swift
+++ hiTour/FeedControllerCell.swift
@@ -7,3 +7,12 @@
 //
 
 import Foundation
+
+import UIKit
+
+class FeedControllerCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageViewFeed: UIImageView!
+    @IBOutlet weak var labelTitle: UILabel!
+    
+}

ca954d6407cbee1cb2499e3d6761d675c2566fcc
Charlie Baker <Charlie.Baker@kcl.ac.uk>
2016-03-16 11:58:23 +0000

    Fix Bug with checking for 3G connection

--- hiTour/Reachability.swift
+++ hiTour/Reachability.swift
@@ -15,23 +15,19 @@ class Reachability {
     ///  Function that trys to access the internet and returns a boolean if it has connectivity
     ///  to the internet.
     class func isConnectedToNetwork() -> Bool {
-        var zeroAddress = sockaddr(sa_len: 0, sa_family: 0, sa_data: (0,0,0,0,0,0,0,0,0,0,0,0,0,0))
-        zeroAddress.sa_len = UInt8(sizeofValue(zeroAddress))
-        zeroAddress.sa_family = sa_family_t(AF_INET)
-        
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
         let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
-            SCNetworkReachabilityCreateWithAddress(kCFAllocatorDefault, UnsafePointer($0))
+            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
         }
-        
-        var flags: SCNetworkReachabilityFlags = SCNetworkReachabilityFlags(rawValue: 0)
-        if SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) == false {
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
             return false
         }
-        
-        let isReachable = flags == .Reachable
-        let needsConnection = flags == .ConnectionRequired
-        
-        return isReachable && !needsConnection
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
     }
     
 }
